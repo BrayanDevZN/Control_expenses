f8057cb5155e1ac0160c64e7231a8f285c3080ab
Brayan <lsouzadiasvaz@gmail.com>
2026-08-17 13:54:08 -0300

    feat: create tables

---
+++ Control/infra/database/tables.py
@@ -0,0 +1,78 @@
+from logs.log import logger
+
+
+"""
+Cria as tabelas do banco se não existir
+"""
+
+from sqlalchemy import Engine, text
+class TablesDbError(Exception):
+    pass
+
+class TablesDb:
+
+    def __init__(self, engine: Engine)-> None:
+
+        #engine do banco de dados
+        self.eng = engine
+
+
+    #comandos sql apara a criação de cada tabela
+    def _sql(self) -> None:
+
+        self.sql = {
+            "users": """
+                    create table if not exists users(
+                    id serial primary key,
+                    public_id uuid,
+                    name text not null,
+                    password text,
+                    wage numeric(10, 2),
+                    created_at timestamptz current_timestamp
+                    )
+                    """,
+            "expenses": """
+                        create table if not exists expenses( 
+                        id serial primary key,
+                        user_id int,
+                        name text not null,
+                        quantity int not null,
+                        price numeric (10,2),
+                        created_at timestamptz default current_timestamp,
+                        foreign (user_id) references users(id) on delete cascade
+                        )
+                        """     
+        }
+
+    #Executa as querys
+    def _query(self) -> None:
+
+        try:
+
+            with self.eng.begin() as session:
+
+                for table, sql in self.sql.items():
+
+                    logger.info(f"Criando tabela {table} caso não exista...")
+
+                    session.execute(text(sql))
+
+        except Exception as e:
+
+            logger.error(e)
+
+            raise TablesDbError(e)
+
+
+    #Executa os metodos
+    def run(self) -> None:
+
+        self._sql()
+        self._query()
+
+        
+
+    
+
+    
+        
