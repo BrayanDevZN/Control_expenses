83c84ac0b5be599041a57b1a8708f7cdd10037bb
Brayan <lsouzadiasvaz@gmail.com>
2026-08-18 11:15:36 -0300

    feat: create hash

---
+++ Control/domain/hash.py
@@ -0,0 +1,34 @@
+from logs.log import logger
+
+
+"""Cria os hash pra senha e compara"""
+
+import bcrypt
+
+class HashPass:
+
+    #Cria o hash
+    @staticmethod
+    def create(password:str) -> str:
+
+        logger.info("Condificando senha...")
+
+        new_pass = bcrypt.hashpw(password=password.encode(), salt=bcrypt.gensalt())
+
+        return new_pass.decode("utf-8")
+
+
+
+    #Compara os a senha coma senha em hash
+    @staticmethod
+    def valid(password:str, password_hash:str) -> bool:
+
+        logger.info("Conparando senhas...")
+
+        return bcrypt.checkpw(password=password.encode(), hashed_password=password_hash.encode())
+
+
+
+
+
+
