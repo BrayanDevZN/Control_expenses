4cba980c1d0f852f393fb44a97f27107570f4ce3
BrayanDev_Ai <lsouzadiasvaz@gmail.com>
2026-08-19 16:52:49 -0300

    Delete Control directory

--- Control/src/service/encode.py
+++
@@ -1,9 +0,0 @@
-"""
-Junta a chave do jwt com a classe e os modulos
-"""
-from infra.manage import sing
-import domain.module as dm
-
-#Instancias que cuidam da criptografia
-jwt = dm.JwtToken(sing=sing)
-hash = dm.HashPass()
