bd176f5fb2d37a750b5e394a7cfe7207680c26ee
Brayan <lsouzadiasvaz@gmail.com>
2026-08-19 16:06:42 -0300

    finish

--- Control/domain/models/model_expanses.py
+++ Control/domain/models/model_expanses.py
@@ -2,11 +2,32 @@
 Model de expanses
 """
 
-from pydantic import BaseModel
-
+from pydantic import BaseModel, Field, field_validator
+from typing import Literal
 class validExpanse(BaseModel):
 
     public_id: int|str
     name: str
-    quantity: int
-    price:float
+    quantity: int = Field(gt=0)
+    price:float = Field(gt=0)
+
+
+class ValidUpdateExpanses(BaseModel):
+    name:str
+    set:Literal["name", "quantity", "price"]
+    value:str|float|int
+
+
+    #Se value for um int ou float, valida se é maior que zero
+    @field_validator("value")
+    def valid(cls, v):
+
+        if isinstance(v, int) or isinstance(v, float):
+
+            if v <= 0:
+
+                raise ValueError("The value is less than zero")
+
+
+        return v
+
