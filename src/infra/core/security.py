125b2b7c5e4477de34e441289e6ec509c0670d3f
Brayan <lsouzadiasvaz@gmail.com>
2026-08-20 15:29:09 -0300

    fix: debug infra

--- src/infra/core/security.py
+++ src/infra/core/security.py
@@ -5,9 +5,9 @@ Configurações de midllewares
 
 class Users:
 
-    exists = {"methods": ["UPDATE", "DELETE"], "path": "/users/", "ignore": ["/auth/logout/"]}
-    check = {"methods": ["PATCH", "DELETE"], "path": "/users/", "ignore":["/auth/logout/"]}
-    not_exists = {"methods": ["POST"], "path": "/users/", "ignore": ["/auth/logout"]}
-    token= {"methods": ["PATCH", "DELETE"], "path": "/users/", "ignore": ["/auth/logout/"]}
+    exists = {"methods": ["UPDATE", "DELETE"], "path": "/users/", "ignore": ["/auth/"]}
+    check = {"methods": ["PATCH", "DELETE"], "path": "/users/", "ignore":["/auth/"]}
+    not_exists = {"methods": ["POST"], "path": "/users/", "ignore": ["/auth/"]}
+    token= {"methods": ["PATCH", "DELETE"], "path": "/users/", "ignore": ["/auth/"]}
 
 
